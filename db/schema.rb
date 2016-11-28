42b35a19d39df6837d5d705828a42d9e2f21e507
Katelyn Sievert <katelyn.sievert@gmail.com>
2016-11-27 21:20:53 -0800

    Remove unused files

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20161125212313) do
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
